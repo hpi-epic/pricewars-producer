b659b0228ac7d7353edb01062dd0780ab7c37a1b
Johanna Latt <Johanna Latt>
2016-11-08 17:01:47 +0100

    changes merchantID to merchant_id

--- controllers/routes/routes.js
+++ controllers/routes/routes.js
@@ -9,29 +9,29 @@ var appRouter = function(app) {
             return res.status(200).send(RegisteredMerchants.GetRegisteredMerchants());
         })
         .post("/buyers", function(req, res) {
-            if(!req.body.merchantID) {
-                console.log("POST Buyers called without merchantID");
+            if(!req.body.merchant_id) {
+                console.log("POST Buyers called without merchant_id");
 
                 return res.status(400).send({
                     "code": 400,
-                    "message": "missing the merchantID parameter",
-                    "fields" : "constraints violated for field 'merchantID'"
+                    "message": "missing the merchant_id parameter",
+                    "fields" : "constraints violated for field 'merchant_id'"
                 });
             } else {
-                console.log("POST Buyers called with merchant " + req.body.merchantID);
+                console.log("POST Buyers called with merchant " + req.body.merchant_id);
 
                 // check if merchant already exists
-                var registeredMerchant = RegisteredMerchants.GetRegisteredMerchant(req.body.merchantID);
+                var registeredMerchant = RegisteredMerchants.GetRegisteredMerchant(req.body.merchant_id);
                 if (registeredMerchant != undefined) {
                     return res.status(409).send({
                         "code": 409,
-                        "message": "this merchantID is already registered with the producer",
-                        "fields" : "merchantID-body-data"
+                        "message": "this merchant_id is already registered with the producer",
+                        "fields" : "merchant_id-body-data"
                     });
                 }
 
                 // add this merchant and his product selection to the storage
-                RegisteredMerchants.RegisterMerchant(req.body.merchantID, Products.GetRandomProducts(5));
+                RegisteredMerchants.RegisterMerchant(req.body.merchant_id, Products.GetRandomProducts(5));
 
                 res.status(200).send({
                     "code": 200,
@@ -41,15 +41,15 @@ var appRouter = function(app) {
         })
         .delete("/buyers", function(req, res) {
             console.log("DELETE Buyers called");
-            if(!req.body.merchantID) {
+            if(!req.body.merchant_id) {
                 return res.status(400).send({
                     "code": 400,
-                    "message": "missing the merchantID parameter",
-                    "field" : "constraints violated for field 'merchantID'"
+                    "message": "missing the merchant_id parameter",
+                    "field" : "constraints violated for field 'merchant_id'"
                 });
             } else {
                 // try and delete the merchant
-                var deleted = RegisteredMerchants.DeleteMerchant(req.body.merchantID);
+                var deleted = RegisteredMerchants.DeleteMerchant(req.body.merchant_id);
                 if (deleted) {
                     return res.status(200).send({
                         "status": 200,
@@ -60,28 +60,28 @@ var appRouter = function(app) {
                 // merchant didnt exist, couldnt delete
                 return res.status(409).send({
                     "code": 409,
-                    "message": "this merchantID is not registered with the producer",
-                    "field" : "merchantID-body-data"
+                    "message": "this merchant_id is not registered with the producer",
+                    "field" : "merchant_id-body-data"
                 });
             }
         })
         .get("/products/buy", function(req, res) {
-            if(!req.query.merchantID) {
-                console.log("GET Buy Product called without merchantID");
+            if(!req.query.merchant_id) {
+                console.log("GET Buy Product called without merchant_id");
                 return res.status(400).send({
                     "code": 400,
-                    "message": "missing the merchantID form-parameter",
-                    "field" : "constraints violated for field 'merchantID'"
+                    "message": "missing the merchant_id form-parameter",
+                    "field" : "constraints violated for field 'merchant_id'"
                 });
             } else {
-                console.log("GET Buy Product called with merchantID " + req.query.merchantID);
-                var registeredMerchant = RegisteredMerchants.GetRegisteredMerchant(req.query.merchantID);
+                console.log("GET Buy Product called with merchant_id " + req.query.merchant_id);
+                var registeredMerchant = RegisteredMerchants.GetRegisteredMerchant(req.query.merchant_id);
 
                 if (registeredMerchant === undefined) {
                     return res.status(401).send({
                         "code": 401,
                         "message": "merchant is not known to the producer, please register first",
-                        "field" : "please provide a registered merchantID as 'merchantID'-form-parameter"
+                        "field" : "please provide a registered merchant_id as 'merchant_id'-form-parameter"
                     });
                 }
 
