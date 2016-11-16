151278af7f22da94ec985342e824a85795be0ecd
Johanna Latt <Johanna Latt>
2016-11-16 16:03:02 +0100

    * merchant receives completely random product on buy (not just a product from his start stock) * products vary in quality now - for that introduced UID (UID only exists once and is specific to product + quality! product_id is specific to product but not to quality) * removed route for buying specific products (no longer wanted) * changed route products/buy to just /buy (to avoid confusion with products/{product_id})

--- controllers/models/Products.js
+++ controllers/models/Products.js
@@ -1,96 +1,180 @@
 var Products = [
     {
+        uid: 1,
         product_id: 1,
-        name: "CD_Rock",
+        name: "CD_1",
         genre: "Rock",
+        quality: 1,
         price: 15
     },
     {
+        uid: 2,
+        product_id: 1,
+        name: "CD_1",
+        genre: "Rock",
+        quality: 2,
+        price: 12
+    },
+    {
+        uid: 3,
+        product_id: 1,
+        name: "CD_1",
+        genre: "Rock",
+        quality: 3,
+        price: 9
+    },
+    {
+        uid: 4,
+        product_id: 1,
+        name: "CD_1",
+        genre: "Rock",
+        quality: 4,
+        price: 6
+    },
+    {
+        uid: 5,
         product_id: 2,
-        name: "CD_Classic",
+        name: "CD_2",
         genre: "Classic",
+        quality: 1,
         price: 15
     },
     {
+        uid: 6,
+        product_id: 2,
+        name: "CD_2",
+        genre: "Classic",
+        quality: 2,
+        price: 12
+    },
+    {
+        uid: 7,
+        product_id: 2,
+        name: "CD_2",
+        genre: "Classic",
+        quality: 3,
+        price: 9
+    },
+    {
+        uid: 8,
+        product_id: 2,
+        name: "CD_2",
+        genre: "Classic",
+        quality: 4,
+        price: 6
+    },
+    {
+        uid: 9,
         product_id: 3,
-        name: "CD_Pop",
-        genre: "Pop",
+        name: "CD_3",
+        genre: "Jazz",
+        quality: 1,
         price: 15
     },
     {
+        uid: 10,
+        product_id: 3,
+        name: "CD_3",
+        genre: "Jazz",
+        quality: 2,
+        price: 12
+    },
+    {
+        uid: 11,
+        product_id: 3,
+        name: "CD_3",
+        genre: "Jazz",
+        quality: 3,
+        price: 9
+    },
+    {
+        uid: 12,
+        product_id: 3,
+        name: "CD_3",
+        genre: "Jazz",
+        quality: 4,
+        price: 6
+    },
+    {
+        uid: 13,
         product_id: 4,
-        name: "CD_Jazz",
-        genre: "JazZ",
+        name: "CD_4",
+        genre: "Pop",
+        quality: 1,
         price: 15
     },
     {
-        product_id: 5,
-        name: "CD_Children",
-        genre: "Children",
-        price: 15
+        uid: 14,
+        product_id: 4,
+        name: "CD_4",
+        genre: "Pop",
+        quality: 2,
+        price: 12
     },
     {
-        product_id: 6,
-        name: "CD_Electro",
-        genre: "Electro",
-        price: 15
+        uid: 15,
+        product_id: 4,
+        name: "CD_4",
+        genre: "Pop",
+        quality: 3,
+        price: 9
     },
     {
-        product_id: 7,
-        name: "CD_Dance",
-        genre: "Dance",
-        price: 15
-    },
-    {
-        product_id: 8,
-        name: "CD_Country",
-        genre: "Country",
-        price: 15
-    },
-    {
-        product_id: 9,
-        name: "CD_HipHop",
-        genre: "Hip Hop",
-        price: 15
-    },
-    {
-        product_id: 10,
-        name: "CD_Christmas",
-        genre: "Christmas",
-        price: 15
+        uid: 16,
+        product_id: 4,
+        name: "CD_4",
+        genre: "Pop",
+        quality: 4,
+        price: 6
     }
 ];
 
-Products.GetStartProductIDs = function (numberOfRandomProducts, numberOfSharedProducts) {
-    var randomProductIDs = [];
+Products.GetStartProductUIDs = function (numberOfRandomProducts, numberOfSharedProducts) {
+    var randomProductUIDs = [];
 
     // if merchants share products, just add the first x available products to the selection
     if (numberOfSharedProducts === undefined) numberOfSharedProducts = 0;
     for (var i = 0; i < numberOfSharedProducts; i++) {
-        randomProductIDs.push(Products[i].product_id);
+        randomProductUIDs.push(Products[i].uid);
     }
 
     // then fill up with random products
-    while (randomProductIDs.length < (numberOfRandomProducts + numberOfSharedProducts)) {
+    while (randomProductUIDs.length < (numberOfRandomProducts + numberOfSharedProducts)) {
         var randomProductIndex = getRandomInt(numberOfSharedProducts, Products.length - 1);
         var randomProduct = Products[randomProductIndex];
 
         // check whether product has been chosen already
-        if (randomProductIDs.indexOf(randomProduct.product_id) > -1) continue;
+        if (randomProductUIDs.indexOf(randomProduct.uid) > -1) continue;
 
-        randomProductIDs.push(randomProduct.product_id);
+        randomProductUIDs.push(randomProduct.uid);
     }
 
-    return randomProductIDs;
+    return randomProductUIDs;
+};
+
+Products.GetProductByUID = function (uid) {
+    for (var i = 0; i < Products.length; i++) {
+        if (Products[i].uid === uid) {
+            return Products[i];
+        }
+    }
+    return undefined;
 };
 
 Products.GetProductByID = function (product_id) {
     for (var i = 0; i < Products.length; i++) {
         if (Products[i].product_id === product_id) {
+            var productWithoutQuality = Products[i];
+            delete productWithoutQuality.quality;
+            delete productWithoutQuality.uid;
             return Products[i];
         }
     }
     return undefined;
+};
+
+Products.GetRandomProduct = function() {
+    return Products[getRandomInt(0, Products.length - 1)];
 };
 
 // returns a random int (range including min and max)
